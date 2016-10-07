22e1e701f8070b6b33ef812b3a7718be2f9029c6
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-07 09:59:07 -0300

    .

--- app/templates/tasks/client/build_css.js
+++ app/templates/tasks/client/build_css.js
@@ -2,7 +2,10 @@ import gulp from 'gulp';
 import cssmin from 'gulp-clean-css';
 import {base, tasks} from './const';
 
-const CSS = base.DIST + '**/*.css';
+const CSS = [
+  base.DIST + '**/*.css',
+  '!' + base.DIST + 'bower_components/**/*.css',
+];
 
 gulp.task(tasks.CLIENT_BUILD_CSS_DIST, () => {
   return gulp.src(CSS, {base: base.DIST})
--- app/templates/tasks/client/build_image.js
+++ app/templates/tasks/client/build_image.js
@@ -2,10 +2,13 @@ import gulp from 'gulp';
 import imageMin from 'gulp-imagemin';
 import {base, tasks} from './const';
 
-const IMAGES = base.DIST + '**/*.{png,jpg,jpeg,gif}';
+const IMAGES = [
+  base.DIST + '**/*.{png,jpg,jpeg,gif}',
+  '!' + base.DIST + 'bower_components/**/*.{png,jpg,jpeg,gif}',
+]
 
 gulp.task(tasks.CLIENT_IMAGE_DIST, () => {
   return gulp.src(IMAGES, {base: base.DIST})
-		     .pipe(imageMin())
+		         .pipe(imageMin())
              .pipe(gulp.dest(base.DIST));
 });
